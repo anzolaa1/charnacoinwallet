39aab966f96fc7c0c400a1de4476230d2a1699d6
Charna Crypto <info@charnatech.com>
2017-02-23 09:00:59 -0500

    Update CryptoNoteWallet.cmake

--- CryptoNoteWallet.cmake
+++ CryptoNoteWallet.cmake
@@ -1,4 +1,4 @@
 
-set(CN_PROJECT_NAME "")
-set(CN_CURRENCY_DISPLAY_NAME "")
-set(CN_CURRENCY_TICKER "")
+set(CN_PROJECT_NAME "charnacoin")
+set(CN_CURRENCY_DISPLAY_NAME "charnacoin")
+set(CN_CURRENCY_TICKER "CHRC")
